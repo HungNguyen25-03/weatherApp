a326f6e4cdb5300180fa23c3c599453ff4e6cfb9
thienhung27hao@gmail.com <thienhung27hao@gmail.com>
2023-12-08 21:50:28 +0700

    complete layout

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -3,6 +3,7 @@ import "./WeatherApp.css";
 import search_icon from "../Assets/search.png";
 import clound_icon from "../Assets/cloud.png";
 import humidity from "../Assets/humidity.png";
+import wind from "../Assets/wind.png";
 
 const WeatherApp = () => {
   return (
@@ -13,14 +14,25 @@ const WeatherApp = () => {
           <img src={search_icon} alt="" />
         </div>
       </div>
-      <div className="weather-icon">
+      <div className="weather-img">
         <img src={clound_icon} alt="" />
       </div>
       <div className="weather-temp">24C</div>
       <div className="weather-location">London</div>
       <div className="data-container">
-        <div className="humidity-icon">
-          <img src={humidity} alt="" />
+        <div className="element">
+          <img src={humidity} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img src={wind} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">18 km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
         </div>
       </div>
     </div>
